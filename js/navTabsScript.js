44ce570f52f178c385e1a24fa80764835adee37b
Nikolai Petukhov <nikolaipetukhov@gmai.com>
2022-05-05 19:15:33 +0300

    create tabs

---
+++ js/navTabsScript.js
@@ -0,0 +1,39 @@
+const tabs = document.querySelectorAll('.tab');
+const tabsContent = document.querySelectorAll('.tabs-content');
+
+function initTabs() {
+    for (let tab of tabs) {
+        if(tab.classList.contains('active-tab')) {
+            tabsContent.forEach(tabContent => {
+                if(tab.dataset.tabId !== tabContent.id)
+                    tabContent.style.display = 'none';
+            });
+        }
+    }
+}
+
+function setActiveContent(tab) {
+    tabsContent.forEach(tabContent => {
+        if (tabContent.id === tab.dataset.tabId) {
+            tabContent.style.display = 'block';
+        } else {
+            tabContent.style.display = 'none';
+        }
+    });
+}
+
+function navTabsFunctionality() {
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            if(!tab.classList.contains('active-tab')) {
+                tabs.forEach(tab => {
+                    tab.classList.remove('active-tab');
+                });
+                tab.classList.add('active-tab');
+                setActiveContent(tab);
+            }
+        })
+    });
+}
+
+export { initTabs, navTabsFunctionality };
